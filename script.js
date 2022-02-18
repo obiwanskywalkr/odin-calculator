37656ee12eb9ea29d06c4436c15b4463c4b29881
obiwanskywalkr <gp47spd13@gmail.com>
2022-02-17 21:43:55 -0600

    Add basic operator functions

---
+++ script.js
@@ -0,0 +1,10 @@
+const operators = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+}
+
+function operate(operator, a, b) {
+    return operator(a, b);
+}
