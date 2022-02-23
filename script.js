d26e8e6848dc70b7918d20e98fd3d6f38f10d29e
obiwanskywalkr <gp47spd13@gmail.com>
2022-02-23 14:34:26 -0600

    Connect everything with eventListeners and update display with values

--- script.js
+++ script.js
@@ -1,10 +1,75 @@
-const operators = {
+const operator = {
     add: (a, b) => a + b,
     subtract: (a, b) => a - b,
     multiply: (a, b) => a * b,
     divide: (a, b) => a / b,
 }
+const display = document.getElementById('display');
+const numbers = document.querySelectorAll('.numberButton');
+const operators = document.querySelectorAll('.operatorButton');
+const equals = document.getElementById('runOperation');
+const allClear = document.getElementById('allClear');
+const clear = document.getElementById('clear');
+let currentValue = [];
+let storedValue = currentValue;
+let storedOperator = [];
+let sum;
 
-function operate(operator, a, b) {
-    return operator(a, b);
+numbers.forEach(number => {
+    number.addEventListener('click', (e) => {
+        currentValue.push(e.currentTarget.textContent);
+        if (currentValue.length >= 7) {
+            display.textContent = parseInt(currentValue.join('')).toExponential(2);
+        } else {
+            display.textContent = currentValue.join('');
+        }
+    });
+});
+operators.forEach(operator => {
+    operator.addEventListener('click', (e) => {
+        storedOperator.push(e.currentTarget.value);
+        currentValue = [];
+    });
+});
+equals.addEventListener('click', () => {
+    operate();
+});
+
+function operate(currentOperator, a, b) {
+    if ((typeof(storedValue) == 'object')) {
+        a = parseInt(storedValue.join(''));
+    } else {
+        a = storedValue;
+    }
+    b = parseInt(currentValue.join(''));
+    if (storedOperator[0] === 'add') {
+        sum = operator.add(a, b);
+        storedValue = sum;
+        storedOperator = [];
+        display.textContent = storedValue;
+    } else if (storedOperator[0] === 'subtract') {
+        sum = operator.subtract(a, b);
+        storedValue = sum;
+        storedOperator = [];
+        display.textContent = storedValue;
+    } else if (storedOperator[0] === 'multiply') {
+        sum = operator.multiply(a, b);
+        storedValue = sum;
+        storedOperator = [];
+        display.textContent = storedValue;
+    } else if (storedOperator[0] === 'divide') {
+        sum = operator.divide(a, b);
+        storedValue = sum;
+        storedOperator = [];
+        display.textContent = storedValue;
+    }
+}
+
+function displaySum(sum) {
+    storedValue = sum;
+    if (storedValue > 999999) {
+        display.textContent = storedValue.toExponential(2);
+    } else {
+        display.textContent = storedValue;
+    }
 }
